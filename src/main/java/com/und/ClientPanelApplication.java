d93fba49d1ae9ff3d924237c9231ffc43918e2cb
Amit Lamba <amitlamba4198@gmail.com>
2017-08-26 17:54:01 +0530

    "First Commit Project Setup"

---
+++ src/main/java/com/und/ClientPanelApplication.java
@@ -0,0 +1,12 @@
+package com.und;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientPanelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientPanelApplication.class, args);
+    }
+}
