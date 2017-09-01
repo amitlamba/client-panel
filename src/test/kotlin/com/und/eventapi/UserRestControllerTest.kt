1951354f7b63ff16e8af7d24f3a8093b45ac9e42
shiv <shivprataps@gmail.com>
2017-09-02 04:58:36 +0530

    fixed test case with optional values provided

--- src/test/kotlin/com/und/eventapi/UserRestControllerTest.kt
+++ src/test/kotlin/com/und/eventapi/UserRestControllerTest.kt
@@ -1,5 +1,6 @@
 package com.und.eventapi
 
+import com.und.common.utils.DateUtils
 import com.und.security.RestTokenUtil
 import com.und.security.RestUserFactory
 import com.und.security.model.Authority
@@ -9,6 +10,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Matchers.any
+import org.mockito.Matchers.anyString
 import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -62,26 +64,45 @@ class UserRestControllerTest {
     @Throws(Exception::class)
     fun getPersonsSuccessfullyWithUserRole() {
 
-        val authority = Authority()
-        authority.id = 1L
-        authority.name = AuthorityName.ROLE_ADMIN
-        val authorities = Arrays.asList(authority)
 
-        val user = User()
-        user.username = "username"
-        user.authorities = authorities
-        user.enabled = java.lang.Boolean.TRUE
-        user.clientSecret = "secret"
-        user.lastPasswordResetDate = Date(System.currentTimeMillis() + 1000 * 1000)
+
+        val user = buildMockUser(AuthorityName.ROLE_ADMIN)
 
         val eventUser = RestUserFactory.create(user)
 
-        `when`<String>(this.restTokenUtil.getUsernameFromToken(any(), any())).thenReturn(user.username)
+        `when`<String>(this.restTokenUtil.getUsernameFromToken(anyString(), anyString())).thenReturn(user.username)
 
-        `when`<UserDetails>(this.userDetailsService.loadUserByUsername(any())).thenReturn(eventUser)
+        `when`<UserDetails>(this.userDetailsService.loadUserByUsername(anyString())).thenReturn(eventUser)
 
         this.mvc!!.perform(get("/user"))
                 .andExpect(status().is2xxSuccessful)
+    }
+
+    private fun buildMockUser(authorityNames: AuthorityName): User {
+
+        val user = User()
+        user.username = "username"
+        user.firstname = "firstname"
+        user.authorities = buildAuthorities(authorityNames)
+        user.enabled = java.lang.Boolean.TRUE
+        user.password = ""
+        user.lastname = ""
+        user.clientSecret = "secret"
+        user.email = ""
+        user.key = ""
+        user.lastPasswordResetDate = DateUtils().now()
+        user.lastPasswordResetDate = Date(System.currentTimeMillis() + 1000 * 1000)
+        return user
+    }
+
+    private fun buildAuthorities(authorityNames: AuthorityName): List<Authority>? {
+
+
+        val authority = Authority()
+        authority.id = 1L
+        authority.name = authorityNames
+        val authorities = Arrays.asList(authority)
+        return authorities
     }
 
 }
