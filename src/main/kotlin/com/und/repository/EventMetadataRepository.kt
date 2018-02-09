e4e7b80e26e30ea5a7246fbb8534e1e095fa2ee8
shiv <shivprataps@gmail.com>
2018-02-09 13:40:56 +0530

    added user properties

--- src/main/kotlin/com/und/repository/EventMetadataRepository.kt
+++ src/main/kotlin/com/und/repository/EventMetadataRepository.kt
@@ -1,13 +1,18 @@
 package com.und.eventapi.repository
 
-import com.und.eventapi.model.Event
-import com.und.eventapi.model.Identity
+import com.und.model.mongo.CommonMetadata
 import com.und.model.mongo.EventMetadata
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface EventMetadataRepository : MongoRepository<EventMetadata, String> {
+
+
+}
+
+@Repository
+interface CommonMetadataRepository : MongoRepository<CommonMetadata, String> {
 
 
 }
