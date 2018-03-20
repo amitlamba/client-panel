063c41db2ff1b8d310d916c8e8c6129938e1f02c
om <lakshbhutani@gmail.com>
2018-03-20 15:51:01 +0530

    "Text area and TinyMCE editor added and Unsubscribe feature"

--- frontend/src/app/templates/email-templates/create-email-template-form/create-email-template-form.component.ts
+++ frontend/src/app/templates/email-templates/create-email-template-form/create-email-template-form.component.ts
@@ -13,14 +13,15 @@ import {UserParams} from "../../../_models/user";
   styleUrls: ['./create-email-template-form.component.css']
 })
 export class CreateEmailTemplateFormComponent implements OnInit, OnChanges {
-
+  emailBodyHtml: string = 'emailBodyHtml';
+  showTextArea: boolean = true;
+  showTinymceEditor: boolean = false;
   showRichTextEditor: boolean = true;
   emailTemplate: EmailTemplate = new EmailTemplate();
   @ViewChild("f") form: any;
 
   userFields = UserFields.USER_DETAIILS;
   items = UserParams.params;
-  suggested:string[]=["laksh","kamal","a","b","c"];
 
   constructor(private templatesService: TemplatesService, private messageService: MessageService) {
   }
@@ -30,11 +31,12 @@ export class CreateEmailTemplateFormComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     this.templatesService.castEmailTemplateForEdit.subscribe((emailTemplateForEdit) => {
-      this.emailTemplate=emailTemplateForEdit;
+      this.emailTemplate = emailTemplateForEdit;
     });
   }
 
   onSave(form: FormData) {
+    console.log(this.emailTemplate);
     if (this.form.valid) {
       if (this.emailTemplate.id) {
         this.templatesService.saveEmailTemplate(this.emailTemplate)
@@ -49,7 +51,7 @@ export class CreateEmailTemplateFormComponent implements OnInit, OnChanges {
           .subscribe(
             response => {
               console.log(response);
-              this.emailTemplate.id=response
+              this.emailTemplate.id = response
               this.templatesService.addEmailTemplate(this.emailTemplate);
               this.messageService.addSuccessMessage("Email Template Created Successfully");
             }
@@ -58,8 +60,33 @@ export class CreateEmailTemplateFormComponent implements OnInit, OnChanges {
     }
   }
 
-  keyupHandlerFunction(event: any) {
-
+  keyupHandler(event: any) {
+    let val: string = "";
+    console.log(event);
+    val = event;
+    console.log(val);
+    this.emailTemplate.emailTemplateBody = val;
+    console.log(this.emailTemplate.emailTemplateBody);
   }
 
+  htmlOptionActive() {
+    this.showTextArea = true;
+    this.showTinymceEditor = false;
+  }
+
+  addSomeText() {
+    this.showTinymceEditor = true;
+    this.showTextArea = false;
+  }
+
+  addUnsubscribeLink(event) {
+    if (event.srcElement.textContent === 'Add Unsubscribe') {
+      document.querySelector('textarea').value = document.querySelector('textarea').value + '<a href="#">Unsubscribe</a>';
+      event.srcElement.textContent = 'Remove Unsubscribe';
+    }
+    else {
+      document.querySelector('textarea').value = document.querySelector('textarea').value.replace('<a href="#">Unsubscribe</a>', '');
+      event.srcElement.textContent = 'Add Unsubscribe';
+    }
+  }
 }
