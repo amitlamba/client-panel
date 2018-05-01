5c9c2af2dd4e9408652a92a2b54954421033f2b1
Kamalpreet Singh <kamalpreet.singh@userndot.com>
2018-05-01 15:19:17 +0530

    Contact Us API

---
+++ src/main/kotlin/com/und/web/model/ContactUs.kt
@@ -0,0 +1,17 @@
+package com.und.web.model
+
+class ContactUs {
+
+    var id:Long? = null
+    var name: String=""
+    var email: String=""
+    var mobileNo: String=""
+    var message: String=""
+
+}
+
+
+
+
+
+
