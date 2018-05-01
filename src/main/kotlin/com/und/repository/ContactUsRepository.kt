88f25109446730b37179fbe79e50d13b6c5fd845
Kamalpreet Singh <kamalpreet.singh@userndot.com>
2018-05-01 15:20:06 +0530

    Contact Us API

---
+++ src/main/kotlin/com/und/repository/ContactUsRepository.kt
@@ -0,0 +1,10 @@
+package com.und.repository
+
+import com.und.model.jpa.ContactUs
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ContactUsRepository: JpaRepository<ContactUs,Long>{
+
+}
