3de2280a2f68156efae18f78ce206498d3ab1b34
shiv <shivprataps@gmail.com>
2018-05-20 02:29:07 +0530

    removed lateinit

--- src/main/kotlin/com/und/web/model/ContactUs.kt
+++ src/main/kotlin/com/und/web/model/ContactUs.kt
@@ -9,19 +9,19 @@ class ContactUs {
 
     var id: Long? = null
 
-    @Size(min=2,max=100,message="{contactUS.name.invalidSize}")
-    @Pattern(regexp = "[A-Za-z][a-zA-Z\\s]*",message = "{contactUS.name.invalid}")
-    lateinit var name: String
+    @Size(min = 2, max = 100, message = "{contactUS.name.invalidSize}")
+    @Pattern(regexp = "[A-Za-z][a-zA-Z\\s]*", message = "{contactUS.name.invalid}")
+    var name: String = ""
 
-    @Email(message="{contactUS.email.invalid}")
-    lateinit var email: String
+    @Email(message = "{contactUS.email.invalid}")
+    var email: String = ""
 
-    @Size(min=10,max=15,message = "{contactUS.mobileNo.invalidSize}")
-    @Pattern(regexp = "[0-9]*",message = "{contactUS.mobileNo.invalidDigits}")
-    lateinit var mobileNo: String
+    @Size(min = 10, max = 15, message = "{contactUS.mobileNo.invalidSize}")
+    @Pattern(regexp = "[0-9]*", message = "{contactUS.mobileNo.invalidDigits}")
+    var mobileNo: String = ""
 
-    @Size(min=50,max=500,message="{contactUS.message.invalidSize}")
-    lateinit var message: String
+    @Size(min = 50, max = 500, message = "{contactUS.message.invalidSize}")
+    var message: String = ""
 
 }
 
